9caf3bc63d697271364556dc4a0db809ae90839f
Wesley Ting <wesleyting3@gmail.com>
2023-04-03 23:43:23 -0700

    removed typography

--- src/template/Header.js
+++ src/template/Header.js
@@ -4,7 +4,6 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
 function Header(props) {
